48c437cb415aab7e946f16969f7e52eca5dc904c
Shahil2002 <105058333+Shahil2002@users.noreply.github.com>
2024-02-07 13:59:16 +0000

    code discussion done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -7,4 +7,8 @@ accountEmail="mohd@google.com"
 accountPassword ="1078"
 accountCity="Faizabad"
 console.log("accountId")
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
 console.table([accountId,accountEmail,accountPassword,accountCity])
