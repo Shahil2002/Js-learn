c468a23c111643a9334602e48698fef24e0aa14d
Shahil2002 <105058333+Shahil2002@users.noreply.github.com>
2024-02-07 13:53:46 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=155423
+let accountEmail="mohd@google.com"
+var accountPassword ="1078"
+accountCity="Ayodhya"
+// account =2  // not allowed
+accountEmail="mohd@google.com"
+accountPassword ="1078"
+accountCity="Faizabad"
+console.log("accountId")
+console.table([accountId,accountEmail,accountPassword,accountCity])
