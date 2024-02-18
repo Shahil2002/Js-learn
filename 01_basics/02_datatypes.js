f58c4fc0d941decfa435057a872b0ca396ae2f29
Shahil2002 <105058333+Shahil2002@users.noreply.github.com>
2024-02-18 14:46:41 +0000

    code discussion done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all JS node as newer version
+// alert(3+3) // we are using node js, not browser
+console.log(3
+    +3) // code redability should be high
+
+console.log("Shahil")
+let name="hitesh"
+let age =18
+let isLoggedIn= false
+// number => 2 to power 53
+// BigInt
+//String=> ""
+//Boolean =>true/false
+//null
