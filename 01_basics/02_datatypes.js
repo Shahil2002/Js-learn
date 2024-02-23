e8f04c02ccb90213f4c92e82c59acb848612c9fd
Shahil2002 <105058333+Shahil2002@users.noreply.github.com>
2024-02-23 16:19:41 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -7,8 +7,16 @@ console.log("Shahil")
 let name="hitesh"
 let age =18
 let isLoggedIn= false
+let state;
 // number => 2 to power 53
 // BigInt
 //String=> ""
 //Boolean =>true/false
-//null
+//null =>standalone value
+//Undefine =>
+//symbol=> unique
+
+
+// object
+console.log("typeof undefined");// undefined
+console.log("typeof null");// object
